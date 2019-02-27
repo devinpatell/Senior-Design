14ec86a08a8531bce743155c550ef0d2f302a1b4
Aadit Panchal <aaditsp@uci.edu>
2019-02-26 17:48:48 -0800

    keyboard bound nicely - dad

--- feed.py
+++ feed.py
@@ -1,9 +1,78 @@
 import CoDrone
 drone = CoDrone.CoDrone()
-drone.pair(drone.Nearest)
+drone.connect()
 
 import numpy as np
 import cv2
+
+## wifi password 12345678
+
+
+
+
+def leftKey():
+    print( "Left key pressed")
+    drone.set_roll(-60)
+    drone.move(.4)
+
+
+def rightKey():
+    print( "Right key pressed")
+    drone.set_roll(40)
+    drone.move(.4)
+
+def upKey():
+    print( "up key pressed")
+    drone.set_pitch(40)
+    drone.move(.4)
+
+
+def downKey():
+    print( "down key pressed")
+    drone.set_pitch(-40)
+    drone.move(.4)
+
+def wKey():
+    print( "w key pressed")
+    drone.set_throttle(100)
+    drone.move(.4)
+
+def sKey():
+    print( "s key pressed")
+    drone.set_throttle(-80)
+    drone.move(.4)
+
+
+def aKey():
+    print( "a key pressed")
+    drone.set_yaw(-40)
+    drone.move(.4)
+
+def dKey():
+    print( "d key pressed")
+    drone.set_yaw(40)
+    drone.move(.4)
+
+
+
+
+def takeoff():
+    print("take off like kodak black")
+    drone.takeoff()
+
+def land():
+    print("landing")
+    drone.set_throttle(-20)
+    drone.move(1)
+    drone.land()
+
+runkey = { 255: None, ord('d') :dKey,32: takeoff, ord('t') : land, ord('i') : upKey,
+          ord('j') : leftKey, ord('k') : downKey, ord('l') : rightKey, ord('w') : wKey, ord('s') : sKey,
+          ord('a') : aKey}
+
+
+
+
 
 #Capture video from the Wifi Connection to FPV module
 #RTSP =(Real Time Streaming Protocol)
@@ -11,7 +80,7 @@ cap = cv2.VideoCapture('rtsp://192.168.100.1/cam1/mpeg4')
 # cap = cv2.VideoCapture(0)
 fps = cv2.CAP_PROP_FPS
 #Create a while loop to grab frame by frame of video
-while (cap.isOpened()):#while capturing video repeat this loop
+while (True):#while capturing video repeat this loop
     #save the video frame read into variable "frame"
     ret, frame = cap.read()
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -28,15 +97,23 @@ while (cap.isOpened()):#while capturing video repeat this loop
     #In order to quit the while loop click q or Q
     #to break out of the loop
     key = cv2.waitKey(1) & 0xFF
-    # print(key)
-    if key == ord('q'):
-        cv2.imwrite('C:\\Users\\devin\\Documents\\Year 4\\Fall 2018\\Senior Design\\feed_image.jpg',frame)
+    if key == ord('q') or key == ord('Q'):
         break
+    else:
+        func = runkey.get(key)
+        if func:
+            func()
+
+
+
 
 
 
 #Relase the camera
 cap.release()
+drone.disconnect()
+print("drone was disconnected")
+
 
 #close all the windows generated
 cv2.destroyAllWindows()
